835477e348e46eea6603ab008a715d0f4357f1f4
vaddy_b <eagle-vadim@mail.ru>
2023-10-12 23:33:13 +0700

    create 2 repo-s and add into basic logic

---
+++ src/main/java/ru/babich/planer/repo/TaskRepository.java
@@ -0,0 +1,21 @@
+package ru.babich.planer.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.babich.planer.enity.Task;
+import ru.babich.planer.enity.User;
+import ru.babich.planer.enity.WorkingPlace;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task,Long> {
+
+    Optional <Task> findByWorkingPlaceOrderByDateOfAdding(WorkingPlace workingPlace);
+
+    Optional <Task> findByUser(User user);
+
+    Optional <Task> findTaskByIdAndUser(Long id, User user);
+
+}
